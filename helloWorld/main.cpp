2d4ed04d31575af275a0262a08564f9944ae39f9
snissim123 <sandra.nissim@gmail.com>
2018-04-27 18:05:42 -0500

    Initial Commit

---
+++ helloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  helloWorld
+//
+//  Created by Sandra Nissim on 4/27/18.
+//  Copyright © 2018 Sandra Nissim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
